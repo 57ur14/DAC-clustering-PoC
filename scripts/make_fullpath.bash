36735ee739d532061af0938d20d2ba9936b4abe4
Sturla <33401295+57ur14@users.noreply.github.com>
2020-10-27 20:24:55 +0100

    Added comments describing purpose and usage

--- scripts/make_fullpath.bash
+++ scripts/make_fullpath.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-base_dir="/home/sturla/IJCNN_10000files/"
+# Used to assist creating full paths for files. This is done by joining a base directory with given input.
+# Input can either come from stdin or be contained in a file that is given as argument to this script.
+
+# Base directory, the path that should be prepended to the lines in the input
+base_dir="/some/path/to/data_set/"
 
 while read line
 do
