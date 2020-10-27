7e9ff0759636de4cfaa7bdd13e40ca7b3e80286d
Sturla <33401295+57ur14@users.noreply.github.com>
2020-10-27 20:15:53 +0100

    Deleted old file that was used during testing

--- scripts/stop.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "$(ps aux | grep qdaemon | grep -v grep)" ]; then
-    echo "Stopping queue daemon"
-    pid=$(ps aux | grep qdaemon | grep -v grep | awk '{print $2}')
-    kill $pid
-fi
-
-# Clustering daemon should quit automatically when queue killed
