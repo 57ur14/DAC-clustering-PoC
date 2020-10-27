7b23a8f09c750de35e8b5989db060f45229e73fa
Sturla <33401295+57ur14@users.noreply.github.com>
2020-10-27 20:19:29 +0100

    Deleted file used for testing

--- scripts/make_combined.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while read  line
-do
-    family=$(echo "$line" | cut -d / -f 5) # Extract family from path in data set
-    echo $line $family                     # Print with correct format
-done < "${1:-/dev/stdin}"
